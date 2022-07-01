42972aa89ce7bb67eceab1ba324df2bd2a4b05cf
Danny Molina Morales <dmolina@iguanait.com>
2022-07-01 16:08:15 -0400

    new project structure

--- tests/unit_tests_web_service.py
+++ tests/unit_tests_web_service.py
@@ -1,7 +1,28 @@
+# -*-coding:utf-8 -*-
+"""
+File    :   unit_tests_web_service.py
+Date    :   2022/07/01 16:07:00
+Author  :   Ing. Danny Molina Morales
+Version :   1.0
+E-mail  :   mmdanny89@gmail.com
+"""
+
+
+import importlib.machinery
+import importlib.util
+from pathlib import Path
+
 import unittest
-import server_api
 import cherrypy
 import requests
+ 
+script_dir = Path( __file__ ).parent.parent
+server_api_path = str( script_dir.joinpath('server_api.py' ) )
+loader = importlib.machinery.SourceFileLoader( 'server_api', server_api_path    )
+spec = importlib.util.spec_from_loader( 'server_api', loader )
+server_api = importlib.util.module_from_spec( spec )
+loader.exec_module( server_api )
+
 
 
 class TestRestApi(unittest.TestCase):
